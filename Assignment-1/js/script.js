a89bc2c022cb289fc44841ea628331b5979d197f
MachinistHamilton <machinisthamilton@gmail.com>
2025-06-11 13:29:03 -0400

    - added js file with arrays

---
+++ Assignment-1/js/script.js
@@ -0,0 +1,6 @@
+// Array of story parts
+const storyPart1 = ["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"];
+const storyPart2 = ["sat on","ate", "danced with", "saw", "doesn't like", "kissed",];
+const storyPart3 = ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"];
+const storyPart4 = ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"];
+const storyPart5 = ["on the moon", "on the chair", "in my spaghetti", "in my soup", "on the grass", "in my shoe"];
