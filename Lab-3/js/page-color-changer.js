d0b9c4830fb45db52d4e099adc9dc66678e5b108
MachinistHamilton <machinisthamilton@gmail.com>
2025-06-11 13:53:01 -0400

    added event listers

--- Lab-3/js/page-color-changer.js
+++ Lab-3/js/page-color-changer.js
@@ -8,3 +8,9 @@ const blueSlider = document.getElementById("blue");
 // grab the body element from html
 const body = document.querySelector("body");
 
+
+// Update color on slider input
+redSlider.addEventListener('input', updateBackgroundColor);
+greenSlider.addEventListener('input', updateBackgroundColor);
+blueSlider.addEventListener('input', updateBackgroundColor);
+
