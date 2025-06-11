222ea6a83e57485c8453f52fe9fb93474e17fd6f
MachinistHamilton <machinisthamilton@gmail.com>
2025-06-11 13:48:25 -0400

    grab all the elements on the html documnet

---
+++ Lab-3/js/page-color-changer.js
@@ -0,0 +1,10 @@
+// grab red slider from html
+const redSlider = document.getElementById("red");
+// grab green slider from html
+const greenSlider = document.getElementById("green");
+// grab blue slider from html
+const blueSlider = document.getElementById("blue");
+
+// grab the body element from html
+const body = document.querySelector("body");
+
