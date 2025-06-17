1a29cf40558f8a8ca2d168e72df17ba21951b399
Thomas <thomasjgouzecky@gmail.com>
2025-06-16 20:30:44 -0400

    removed the hard coded arrays of text because theyre unnessary

--- Assignment-1/js/script.js
+++ Assignment-1/js/script.js
@@ -1,39 +1,4 @@
 //-- VARIABLES --
-// Arrays of story parts
-const storyPart1 = [
-	"The turkey",
-	"Mom",
-	"Dad",
-	"The dog",
-	"My teacher",
-	"The elephant",
-	"The cat",
-];
-const storyPart2 = [
-	"sat on",
-	"ate",
-	"danced with",
-	"saw",
-	"doesn't like",
-	"kissed",
-];
-const storyPart3 = [
-	"a funny",
-	"a scary",
-	"a goofy",
-	"a slimy",
-	"a barking",
-	"a fat",
-];
-const storyPart4 = ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"];
-const storyPart5 = [
-	"on the moon",
-	"on the chair",
-	"in my spaghetti",
-	"in my soup",
-	"on the grass",
-	"in my shoe",
-];
 
 // Variable to keep track of the current index of each array
 let Part1_index = 0;
